43c553bcbf3e0f60ef9574f0520f4a2972956bfe
MinhTri <ngminhtri2203@gmail.com>
2025-02-24 03:51:03 +0700

    AO4

--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  TextInput,
+} from 'react-native';
+import {IconButton} from 'react-native-paper';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen: React.FC<{navigation: any}> = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerText}>Home Page</Text>
+        <IconButton
+          icon={({size, color}) => (
+            <Ionicons name="arrow-back-outline" size={size} color={color} />
+          )}
+          onPress={() => navigation.goBack()}
+        />
+        <TextInput style={styles.searchInput} placeholder="Search" />
+        <IconButton
+          icon={({size, color}) => (
+            <Ionicons name="notifications-outline" size={size} color={color} />
+          )}
+          onPress={() => console.log('Notification pressed')}
+        />
+        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+          <Image
+            source={{uri: 'https://via.placeholder.com/50'}}
+            style={styles.profileImage}
+          />
+        </TouchableOpacity>
       </View>
       <View style={styles.middle}></View>
-      <View style={styles.footer}>
-        <TouchableOpacity
-          style={styles.loginButton}
-          onPress={() => navigation.navigate('Login')}>
-          <Text style={styles.loginButtonText}>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.profileButton}
-          onPress={() => navigation.navigate('Profile')}>
-          <Text style={styles.profileButtonText}>Profile</Text>
-        </TouchableOpacity>
-      </View>
+      <View style={styles.footer}></View>
     </View>
   );
 };
@@ -30,15 +45,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff', // Màu nền trắng
   },
   header: {
-    flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#6363c9', // Màu nền xanh
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    backgroundColor: '#e4e4eb', // Màu nền xanh
+    height: 60, // Điều chỉnh chiều cao cho icon hiển thị đúng
   },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#fff', // Màu chữ trắng
+  searchInput: {
+    flex: 1,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 8,
+    marginHorizontal: 10,
   },
   middle: {
     flex: 2,
@@ -49,30 +70,12 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
     paddingRight: 16,
   },
-  loginButton: {
-    backgroundColor: '#fff', // Nền màu trắng
-    padding: 10,
-    borderRadius: 5,
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
     borderWidth: 1,
     borderColor: 'gray',
-  },
-  loginButtonText: {
-    color: '#000', // Chữ đen
-    fontSize: 16,
-    fontWeight: 'bold', // Chữ in đậm
-  },
-  profileButton: {
-    backgroundColor: '#fff', // Nền màu trắng
-    padding: 10,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'gray',
-    marginTop: 10,
-  },
-  profileButtonText: {
-    color: '#000', // Chữ đen
-    fontSize: 16,
-    fontWeight: 'bold', // Chữ in đậm
   },
 });
 
